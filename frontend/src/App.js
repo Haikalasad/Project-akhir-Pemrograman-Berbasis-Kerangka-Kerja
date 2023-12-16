b3837ddb475e9b482823c70bc17fc16281cfb650
elmasaskiaa <124068316+elmasaskiaa@users.noreply.github.com>
2023-12-16 22:45:35 +0700

    menyelesaikan halaman login (#17)
    Co-authored-by: Muhammad Haikal Fikri As'ad <138842668+Haikalasad@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Login from './pages/Login';
 import DetailPage from './pages/DetailPage';
 import Eksplor from './pages/eksplor';
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/detail/:id" element={<DetailPage />} />
         <Route path="/explore" element={<Eksplor />} />
 
